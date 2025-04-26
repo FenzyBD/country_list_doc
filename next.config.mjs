d2879f5ac4c8eba3f800e615292bf3b185467525
Kawsar <diu.kawsar@gmail.com>
2025-04-26 13:34:21 +0600

    action code update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  // Set the base path to match your repository name
+  basePath: process.env.NODE_ENV === "production" ? "/country_list_doc" : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
